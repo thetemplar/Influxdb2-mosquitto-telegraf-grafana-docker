4d87102b1d823127fa786c2ea4f1d85f00306686
Kristian Graul <kristian@tensor.gmbh>
2022-08-16 11:39:32 +0200

    init

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p /opt/mqtt-server/mosquitto/data /opt/mqtt-server/mosquitto/log opt/mqtt-server/influxdb-conf /opt/mqtt-server/influxdb /opt/mqtt-server/grafana
+sudo chown -R 1883:1883 /opt/mqtt-server/mosquitto
+sudo chown -R 472:472 /opt/mqtt-server/grafana
+
+
+
+
+
+
+
